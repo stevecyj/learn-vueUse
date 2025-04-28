38a7a401729e56c5adc16e66e25485b2cf169100
steve.tsao <steve.tsao@hlwtech.com.tw>
2025-04-28 17:57:04 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useMouse, useTitle, useEventListener } from '@vueuse/core'
+import { reactive, onUnmounted } from 'vue'
+
+const mouse = reactive(useMouse())
+const title = useTitle('Right')
+
+useEventListener('mousemove', (e) => {
+  console.log(e.clientX, e.clientY)
+})
+
+console.log(mouse.x, mouse.y)
+onUnmounted(() => {
+  console.log('unmounted')
+})
+</script>
+
+<template>
+  <main>
+    <h1>{{ title }}</h1>
+  </main>
+</template>
