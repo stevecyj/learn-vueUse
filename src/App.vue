6104bf2ab58f9b97bb2ef8a4311567c11fd84620
stevecyj <ycsteven.tsao@gmail.com>
2025-04-28 23:15:28 +0800

    feat: 增加點擊外部區域的功能
    - 引入 @vueuse/core 和 @vueuse/components，實現點擊外部區域的事件處理
    - 使用 OnClickOutside 組件包裹 HelloWorld，並在點擊外部時觸發 handleClickOutside 函數
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue'
+import { onClickOutside } from '@vueuse/core'
+import { OnClickOutside } from '@vueuse/components'
+
 import HelloWorld from './components/HelloWorld.vue'
+const targetElement = ref(null)
+
+const handleClickOutside = () => {
+  console.log('clicked outside')
+}
+
+onClickOutside(targetElement, handleClickOutside)
 </script>
 
 <template>
@@ -8,7 +19,9 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <OnClickOutside @trigger="handleClickOutside">
+        <HelloWorld msg="You did it!" />
+      </OnClickOutside>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
